209f003a25b5fcf456444104dacceaf3069fb470
mariaSmartDesign <creativitatidisseny@gmail.com>
2023-10-06 12:19:49 +0200

    ex3-visibles las dos respuestas

--- ex3.js
+++ ex3.js
@@ -2,8 +2,8 @@
  * Jugando con string
  */
 
-let nombre = "Maria";
-let apellido = "Aranda";
+let nombre = "Mad";
+let apellido = "Max";
 
 let presentacion = "Me llamo " + nombre +" "+ apellido;
 
@@ -22,4 +22,9 @@ console.log("Pregunta 1:", presentacion);
  * Pregunta 2: Cambia el valor de las variables adecuadamente para que, en vez de "Me llamo Mad Max", escribe "Me llamo {tu nombre} {tu apellido}"
  */
 
-console.log("Pregunta 2:", presentacion);
+let minombre = "Maria";
+let miapellido = "Aranda";
+
+let presentacion2 = "Me llamo " + minombre +" "+ miapellido;
+
+console.log("Pregunta 2:", presentacion2);
