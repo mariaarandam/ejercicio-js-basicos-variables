8f935c0552d688a8cb9c8a9fe654aaf9f0cc94f6
mariaSmartDesign <creativitatidisseny@gmail.com>
2023-10-06 11:30:28 +0200

    ex2

--- ex2.js
+++ ex2.js
@@ -7,13 +7,20 @@
  let variable3 = 3;
  let variable4 = 4;
  variable2= 2;
- varialbe3= 4;
+ variable3= 4;
  variable4= 1;
  variable1= -7;
+
 
  /**
   * En este punto, que valor tendrá 'variable4'. ¿Por qué? ¿Cómo podrías comprobarlo?
   */
+
+ /* Respuesta*/
+  console.log("El valor de la 'variable4' es: ");
+  console.log(variable4)
+
+
 
   variable2 = variable1;
   variable4 = variable2 * 2;
@@ -21,3 +28,7 @@
 /**
   * En este punto, que valor tendrá 'variable4'. ¿Por qué? ¿Cómo podrías comprobarlo?
   */
+ 
+ /* Respuesta*/
+ console.log("Segunda opción. El valor de la 'variable4' es: ");
+ console.log(variable2 * 2)
